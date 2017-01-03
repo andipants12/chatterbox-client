0ed2e42b6a45887d063685b381d4b6af8e98e545
andipants12 <andreareneeo@gmail.com>
2017-01-02 16:35:14 -0800

    with this commit we intend to continue developing our chatterbox functionality to: post messages that are input by a specific user

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,46 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+  $('.username').on('click', app.handleUsernameClick);
+  $('.submit').on('submit', app.handleSubmit);
+};
+
+app.send = function(param) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(param)
+  });
+};
+
+app.fetch = function(param) {
+  $.ajax({
+    type: 'GET',
+    data: JSON.stringify(param)
+  });
+};
+
+app.clearMessages = function() {
+  return $('#chats').remove();
+};
+
+app.renderMessage = function(message) {
+  if (!document.getElementById('chats')) {
+    $('body').append('<div id="chats"> </div>');
+  }
+  $('#chats').append('<div>' + message.text + '</div>');
+  //mvp this passes the test, may need to append in a different funcion
+  $('#main').append('<div class="username"></div>');
+};
+
+app.renderRoom = function (room) {
+  if (!document.getElementById('roomSelect')) {
+    $('body').append('<div id="roomSelect"> </div>');
+  }
+  $('#roomSelect').append('<div>' + room + '</div>');
+};
+
+app.handleUsernameClick = function() {};
+app.handleSubmit = function() {};
+
